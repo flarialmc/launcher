8a7c145d8025c9d1fce24b0a92c366a6c9286f29
Bari <58800830+BariPlayzYT@users.noreply.github.com>
2023-01-25 21:50:08 +0600

    w

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int Time = Int32.Parse(DateTime.Now.ToString("HH", System.Globalization.DateTimeFormatInfo.InvariantInfo));
+            if (Time >= 0 && Time < 12) { GreetingLabel.Content = "Good Morning!"; }
+            else if (Time >= 12 && Time < 18) { GreetingLabel.Content = "Good Afternoon!"; }
+            else if (Time >= 18 && Time <= 24) { GreetingLabel.Content = "Good Evening!"; }
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e) => this.DragMove();
+        private void CloseWindow(object sender, RoutedEventArgs e) => this.Close();
+        private void HideGrid(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Visibility = Visibility.Visible;
+            LoginGrid.Visibility = Visibility.Hidden;
+        }
+    }
+}
