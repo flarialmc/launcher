668295f0b37d20fcbbbe17d90899acd13edd3e66
unknown <zohoraf3@gmail.com>
2023-07-15 03:35:48 +0600

    Uh among us

--- WpfApp5/Handlers/Managers/VersionManagement.cs
+++ WpfApp5/Handlers/Managers/VersionManagement.cs
@@ -1,16 +1,14 @@
-﻿using Octokit;
+﻿using Newtonsoft.Json.Linq;
+using Octokit;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
-using System.Runtime.ExceptionServices;
-using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using Windows.Management.Deployment;
-using Newtonsoft.Json.Linq;
 using Application = System.Windows.Application;
 
 namespace Flarial.Launcher.Managers
@@ -26,6 +24,72 @@ namespace Flarial.Launcher.Managers
             public string Description { get; set; }
         }
 
+        public static long GetTotalDownloadableBytes(string url)
+        {
+            try
+            {
+                // Create a web request to the specified URL
+                WebRequest request = WebRequest.Create(url);
+
+                // Get the response from the web server
+                using (WebResponse response = request.GetResponse())
+                {
+                    // Get the content length from the response headers
+                    if (long.TryParse(response.Headers.Get("Content-Length"), out long contentLength))
+                    {
+                        return contentLength;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while retrieving the content length: {ex.Message}");
+            }
+
+            return 0; // Return 0 if the content length cannot be determined or an error occurs
+        }
+        public static long GetTotalBytesOfFile(string filePath)
+        {
+            try
+            {
+                // Check if the file exists
+                if (File.Exists(filePath))
+                {
+                    // Get the file info
+                    FileInfo fileInfo = new FileInfo(filePath);
+
+                    // Return the length (total bytes) of the file
+                    return fileInfo.Length;
+                }
+                else
+                {
+                    Console.WriteLine($"File '{filePath}' does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while getting the file size: {ex.Message}");
+            }
+
+            return 0; // Return 0 if the file size cannot be determined or an error occurs
+        }
+        public static async Task deleteCorrupted()
+        {
+            string path = Path.Combine(launcherPath, "Versions");
+            WebClient webClient = new WebClient();
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                Trace.WriteLine(file);
+
+                string uselessStuffRemoved = file.ToLower().Replace("minecraft", "").Replace(".appx", "");
+                if (GetTotalBytesOfFile(file) < GetTotalDownloadableBytes(ExtractUrl(await webClient.DownloadStringTaskAsync(new Uri($"https://api.jiayi.software/api/v1/minecraft/download_url?version={uselessStuffRemoved}&arch=x64")))))
+                {
+                    Trace.WriteLine(uselessStuffRemoved);
+                }
+            }
+
+        }
         public static async Task<List<string>> GetVersionsAsync()
         {
             try
@@ -48,7 +112,7 @@ namespace Flarial.Launcher.Managers
                 return null;
             }
         }
-        
+
         public static string ExtractUrl(string jsonString)
         {
             // Parse the JSON string into a JObject
@@ -64,9 +128,9 @@ namespace Flarial.Launcher.Managers
         {
             string result = "";
             WebClient webClient = new WebClient();
-            if (version == "1.20.0") result = ExtractUrl(webClient.DownloadStringTaskAsync(new Uri("https://api.jiayi.software/api/v1/minecraft/download_url?version=1.20.0.1&arch=x64")).Result);
+            if (version == "1.20.0.1") result = ExtractUrl(webClient.DownloadStringTaskAsync(new Uri("https://api.jiayi.software/api/v1/minecraft/download_url?version=1.20.0.1&arch=x64")).Result);
             if (version == "1.20.10") result = ExtractUrl(webClient.DownloadStringTaskAsync(new Uri("https://api.jiayi.software/api/v1/minecraft/download_url?version=1.20.10.1&arch=x64")).Result);
-            
+
             Trace.WriteLine(version);
 
             return result;
@@ -109,18 +173,18 @@ namespace Flarial.Launcher.Managers
         }
         public static async Task<bool> InstallAppBundle(string dir)
         {
-            
+
             Trace.WriteLine("called installappbundle");
             Trace.WriteLine(dir);
             try
             {
                 var packageUri = new Uri(Path.Combine(dir, "AppxManifest.xml"));
                 Trace.WriteLine(packageUri.ToString());
-                
-                
+
+
                 File.Delete(Path.Combine(dir, "AppxSignature.p7x"));
                 var packageManager = new PackageManager();
-                
+
                 var addPackageOperation = packageManager.RegisterPackageAsync(packageUri, null, DeploymentOptions.ForceUpdateFromAnyVersion | DeploymentOptions.DevelopmentMode);
                 addPackageOperation.Progress += (sender, args) => ReportProgress(args);
 
@@ -162,8 +226,8 @@ namespace Flarial.Launcher.Managers
         private static void ReportProgress(DeploymentProgress progress)
         {
             // Report the progress of the deployment
-            if(progress.percentage != 100)
-            MainWindow.progressPercentage = (int)progress.percentage;
+            if (progress.percentage != 100)
+                MainWindow.progressPercentage = (int)progress.percentage;
 
             MainWindow.progressType = "Installing";
         }
@@ -171,10 +235,10 @@ namespace Flarial.Launcher.Managers
         private static void DownloadProgressCallback(object sender, DownloadProgressChangedEventArgs e)
         {
             MainWindow.progressType = "download";
-            
-            if(e.ProgressPercentage != 100)
-            MainWindow.progressPercentage = e.ProgressPercentage;
-            
+
+            if (e.ProgressPercentage != 100)
+                MainWindow.progressPercentage = e.ProgressPercentage;
+
             MainWindow.progressBytesReceived = e.BytesReceived;
             MainWindow.progressBytesTotal = e.TotalBytesToReceive;
             Trace.WriteLine($"Downloaded {e.BytesReceived} of {e.TotalBytesToReceive} bytes. {e.ProgressPercentage}% complete...");
@@ -196,7 +260,7 @@ namespace Flarial.Launcher.Managers
 
                 if (removePackageTask.Status == TaskStatus.RanToCompletion)
                 {
-                    
+
                     Trace.WriteLine("Package removal succeeded!");
                     return true;
                 }
@@ -250,11 +314,11 @@ namespace Flarial.Launcher.Managers
                 }
 
                 WebClient webClient = new WebClient();
-                
-                if(File.Exists( Path.Combine(launcherPath, "Versions", version, "UAP.Assets", "minecraft", "icons", "MCSplashScreen.scale-200.png")))
-                    File.Delete( Path.Combine(launcherPath, "Versions", version, "UAP.Assets", "minecraft", "icons", "MCSplashScreen.scale-200.png"));
-                
-                if(File.Exists(Path.Combine(launcherPath, "Versions", version, "data", "resource_packs", "vanilla", "textures",
+
+                if (File.Exists(Path.Combine(launcherPath, "Versions", version, "UAP.Assets", "minecraft", "icons", "MCSplashScreen.scale-200.png")))
+                    File.Delete(Path.Combine(launcherPath, "Versions", version, "UAP.Assets", "minecraft", "icons", "MCSplashScreen.scale-200.png"));
+
+                if (File.Exists(Path.Combine(launcherPath, "Versions", version, "data", "resource_packs", "vanilla", "textures",
                        "ui", "title.png")))
                     File.Delete(Path.Combine(launcherPath, "Versions", version, "data", "resource_packs", "vanilla", "textures",
                         "ui", "title.png"));
@@ -262,9 +326,9 @@ namespace Flarial.Launcher.Managers
                 using (StreamWriter writer = new StreamWriter(Path.Combine(launcherPath, "Versions", version, "data", "resource_packs", "vanilla", "splashes.json"), false))
                 {
                     string jsonString = "{ \"splashes\": [ \"§4Flarial §fMan!\" ] }";
-                        writer.WriteLine(jsonString);
-                    }
-                
+                    writer.WriteLine(jsonString);
+                }
+
                 await webClient.DownloadFileTaskAsync(new Uri("https://cdn.flarial.net/assets/flarial-title.png"),
                     Path.Combine(launcherPath, "Versions", version, "data", "resource_packs", "vanilla", "textures",
                         "ui", "title.png"));
@@ -300,7 +364,7 @@ namespace Flarial.Launcher.Managers
                 Trace.WriteLine("Deploying Minecraft's Application Bundle.");
 
                 await ExtractAppxAsync(path, Path.Combine(launcherPath, "Versions", version), version);
-                
+
                 if (await InstallAppBundle(Path.Combine(launcherPath, "Versions", version)) == false)
                 {
                     CustomDialogBox MessageBox = new CustomDialogBox("Failed", "Failed to install.", "MessageBox");
@@ -321,7 +385,7 @@ namespace Flarial.Launcher.Managers
                 Trace.WriteLine("Installation complete.");
             }
 
-            
+
             isInstalling = false;
             return ello;
         }
