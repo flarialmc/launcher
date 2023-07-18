ab237f85e75af6d3c933574c2bf60141f6274e28
Bari <58800830+TheBarii@users.noreply.github.com>
2023-07-18 14:46:42 +0600

    useless

--- WpfApp5/Handlers/Minecraft/Minecraft.cs
+++ WpfApp5/Handlers/Minecraft/Minecraft.cs
@@ -42,68 +42,14 @@ namespace Flarial.Launcher
 
         public static bool isInstalled()
         {
-            PackageManager = new Windows.Management.Deployment.PackageManager();
+            PackageManager = new PackageManager();
             var Packages = PackageManager.FindPackages(FamilyName);
             return Packages.Count() != 0;
-        }
-        
-        public static bool IsAppxFileCorrupted(string filePath)
-        {
-            try
-            {
-                using (ZipArchive archive = ZipFile.OpenRead(filePath))
-                {
-                    foreach (var entry in archive.Entries)
-                    {
-                        if (entry.Name.Equals("AppxManifest.xml", StringComparison.OrdinalIgnoreCase))
-                        {
-                            using (StreamReader reader = new StreamReader(entry.Open()))
-                            {
-                                // Read the XML content from the AppxManifest.xml file
-                                string xmlContent = reader.ReadToEnd();
-
-                                // Attempt to parse the XML content to check for any parsing errors
-                                XmlDocument xmlDoc = new XmlDocument();
-                                xmlDoc.LoadXml(xmlContent);
-
-                                // If the XML parsing was successful, the APPX file is not corrupted
-                                return false;
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                // An exception occurred while attempting to open or process the APPX file,
-                // indicating that the file is corrupted.
-                return true;
-            }
-
-            // The APPX file is considered corrupted if the AppxManifest.xml entry is not found.
-            return true;
-        }
-
-        public static string CheckAppxFiles(string folderPath)
-        {
-            string[] appxFiles = Directory.GetFiles(folderPath, "*.appx");
-
-            if (appxFiles.Length > 0)
-            {
-                string lastAppxFile = appxFiles.OrderByDescending(f => File.GetLastWriteTime(f)).First();
-
-                if (!IsAppxFileCorrupted(lastAppxFile))
-                {
-                        return lastAppxFile;
-                }
-            }
-
-            return null;
         }
 
         public static void InitManagers()
         {
-            PackageManager = new Windows.Management.Deployment.PackageManager();
+            PackageManager = new PackageManager();
             var Packages = PackageManager.FindPackages(FamilyName);
             
             if (Packages.Count() == 0)
