7949583225a7723548272194d1587af71a5c4071
Bari <58800830+TheBarii@users.noreply.github.com>
2023-07-18 13:46:22 +0600

    remove useless shit

--- Flarial.Minimal/Program.cs
+++ Flarial.Minimal/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.ComponentModel;
-using Microsoft.SqlServer.Server;
 using System.IO;
 using System.Threading;
 using System.Diagnostics;
