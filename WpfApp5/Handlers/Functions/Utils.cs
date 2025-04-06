c266c791101e2e01b4af661a88a3f1670ce2cc11
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-04-06 18:18:20 +0530

    Update dependencies.

--- WpfApp5/Handlers/Functions/Utils.cs
+++ WpfApp5/Handlers/Functions/Utils.cs
@@ -1,17 +1,13 @@
-﻿using Flarial.Launcher.Managers;
-using Microsoft.Win32;
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Security.Principal;
-using Windows.Graphics.Capture;
+﻿using System.Security.Principal;
 
 namespace Flarial.Launcher.Functions
 {
     public class Utils
     {
+        static readonly WindowsPrincipal principal = new(WindowsIdentity.GetCurrent());
+
         public static bool IsGameOpen() => SDK.Minecraft.Installed;
 
-        public static bool IsAdministrator => new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+        public static bool IsAdministrator => principal.IsInRole(WindowsBuiltInRole.Administrator);
     }
 }
