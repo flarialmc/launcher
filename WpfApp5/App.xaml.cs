9dc1f7a70fc00f6fa601da9059e469fb7f4d365b
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-04-12 14:44:12 +0530

    release: Upgrade SDK & single instance the launcher.

--- WpfApp5/App.xaml.cs
+++ WpfApp5/App.xaml.cs
@@ -1,5 +1,18 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 
 namespace Flarial.Launcher;
 
-public partial class App : Application;
+public partial class App : Application
+{
+#pragma warning disable IDE0052
+    static readonly Mutex Mutex;
+#pragma warning restore IDE0052
+
+    static App()
+    {
+        Mutex = new(default, "54874D29-646C-4536-B6D1-8E05053BE00E", out var value);
+        if (!value) Environment.Exit(default);
+    }
+}
