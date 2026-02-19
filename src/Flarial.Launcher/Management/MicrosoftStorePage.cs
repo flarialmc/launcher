77ae991ee4247962261620dd6bddc4db93daca04
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-02-19 23:23:10 +0530

    Remove classes.

--- src/Flarial.Launcher/Management/MicrosoftStorePage.cs
+++ src/Flarial.Launcher/Management/MicrosoftStorePage.cs
@@ -2,20 +2,20 @@ namespace Flarial.Launcher.Management;
 
 abstract class MicrosoftStorePage
 {
-    internal static MinecraftStorePage Minecraft => field ??= new();
-    internal static GamingServicesStorePage GamingServices => field ??= new();
+    internal static MinecraftPage Minecraft => field ??= new();
+    internal static GamingServicesPage GamingServices => field ??= new();
 
     protected abstract string ProductId { get; }
 
     internal void Open() => NativeMethods.ShellExecute($"ms-windows-store://pdp/?ProductId={ProductId}");
 }
 
-sealed class MinecraftStorePage : MicrosoftStorePage
+sealed class MinecraftPage : MicrosoftStorePage
 {
     protected override string ProductId => "9NBLGGH2JHXJ";
 }
 
-sealed class GamingServicesStorePage : MicrosoftStorePage
+sealed class GamingServicesPage : MicrosoftStorePage
 {
     protected override string ProductId => "9MWPM2CQNLHN";
 }
