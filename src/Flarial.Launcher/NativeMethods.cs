048060d43f2b7759f684f1b63e9275057da58621
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-02-19 21:04:37 +0530

    Rename stuff.

--- src/Flarial.Launcher/NativeMethods.cs
+++ src/Flarial.Launcher/NativeMethods.cs
@@ -12,20 +12,6 @@ namespace Flarial.Launcher;
 [System.Security.SuppressUnmanagedCodeSecurity]
 unsafe static class NativeMethods
 {
-    //  const int SW_NORMAL = 1;
-    //   const uint SEM_NOGPFAULTERRORBOX = 0x0002;
-    //  const uint SEM_FAILCRITICALERRORS = 0x0001;
-    //  const uint SEM_NOOPENFILEERRORBOX = 0x8000;
-    //  const uint SEM_NOALIGNMENTFAULTEXCEPT = 0x0004;
-
-    //    [DllImport("Kernel32", SetLastError = true)]
-    //  static extern uint SetErrorMode(uint uMode);
-
-    //  [DllImport("Shell32", CharSet = CharSet.Unicode, SetLastError = true)]
-    //  static extern nint ShellExecute(nint hWnd, string lpOperation, string lpFile, string lpParameters, string lpDirectory, int nShowCmd);
-
-    //  internal static void SetErrorMode() => SetErrorMode(SEM_NOGPFAULTERRORBOX | SEM_FAILCRITICALERRORS | SEM_NOOPENFILEERRORBOX | SEM_NOALIGNMENTFAULTEXCEPT);
-
     internal static void SetErrorMode()
     {
         PInvoke.SetErrorMode(SEM_NOGPFAULTERRORBOX | SEM_FAILCRITICALERRORS | SEM_NOOPENFILEERRORBOX | SEM_NOALIGNMENTFAULTEXCEPT);
@@ -35,15 +21,15 @@ unsafe static class NativeMethods
     {
         fixed (char* lpFile = value)
         {
-            HWND hWnd = HWND.Null;
+            HWND handle = HWND.Null;
 
             if (Application.Current?.MainWindow is { } window)
             {
                 WindowInteropHelper helper = new(window);
-                hWnd = (HWND)helper.EnsureHandle();
+                handle = (HWND)helper.EnsureHandle();
             }
 
-            PInvoke.ShellExecute(hWnd, null, lpFile, null, null, SW_SHOWNORMAL);
+            PInvoke.ShellExecute(handle, null, lpFile, null, null, SW_SHOWNORMAL);
         }
     }
 }
