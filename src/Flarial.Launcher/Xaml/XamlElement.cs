e3a7f3483055080aaa7e475e373900928d5af1ba
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-02-24 11:06:31 +0530

    Cleanup.

--- src/Flarial.Launcher/Xaml/XamlElement.cs
+++ src/Flarial.Launcher/Xaml/XamlElement.cs
@@ -1,10 +1,7 @@
-using Windows.UI.Xaml;
-
 namespace Flarial.Launcher.Xaml;
 
-abstract class XamlElement<T>(T element) where T : UIElement
+abstract class XamlElement<T>(T element) where T : Windows.UI.Xaml.UIElement
 {
     readonly T _element = element;
-
     public static T operator ~(XamlElement<T> element) => element._element;
 }
