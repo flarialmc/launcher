9dd66b878e777b86a2dadf25e1922b776857480d
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-06-01 13:41:11 +0530

    Improve messages for runtime exceptions, again.

--- src/Flarial.Runtime/Exceptions/LibraryLoadFailureException.cs
+++ src/Flarial.Runtime/Exceptions/LibraryLoadFailureException.cs
@@ -4,5 +4,5 @@ namespace Flarial.Runtime.Exceptions;
 
 sealed class LibraryLoadFailureException : Exception
 {
-    internal LibraryLoadFailureException() : base("Failed to load a library, please verify the validity of the library.") { }
+    internal LibraryLoadFailureException() : base("Failed to load a library, please verify its validity.") { }
 }
