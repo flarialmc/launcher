f48e6cc0657bce1758316b0b7e3c5911a1354f71
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-06-01 16:40:15 +0530

    Refactor exception message.

--- src/Flarial.Runtime/Exceptions/LibraryLoadFailureException.cs
+++ src/Flarial.Runtime/Exceptions/LibraryLoadFailureException.cs
@@ -4,5 +4,5 @@ namespace Flarial.Runtime.Exceptions;
 
 sealed class LibraryLoadFailureException : Exception
 {
-    internal LibraryLoadFailureException() : base("Failed to load a library, please verify its validity.") { }
+    internal LibraryLoadFailureException() : base("The provided library cannot be loaded, please verify its validity.") { }
 }
