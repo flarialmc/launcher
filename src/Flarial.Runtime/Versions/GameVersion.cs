8cc469d37e90f1390cd012c20088a2d2cda581d1
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-06-26 13:04:47 +0530

    release: Add bounds checks.

--- src/Flarial.Runtime/Versions/GameVersion.cs
+++ src/Flarial.Runtime/Versions/GameVersion.cs
@@ -1,3 +1,4 @@
+using System;
 using Windows.ApplicationModel;
 
 namespace Flarial.Runtime.Versions;
@@ -9,8 +10,19 @@ unsafe readonly ref struct GameVersion
         var index = 0;
         var segments = stackalloc int[3];
 
-        foreach (var value in version) if (value is '.') ++index;
-        else segments[index] = value - '0' + segments[index] * 10;
+        foreach (var character in version)
+        {
+            if (character is '.')
+            {
+                if (++index >= 3)
+                    throw new FormatException();
+                continue;
+            }
+
+            var digit = character - '0';
+            if (digit > 9) throw new FormatException();
+            segments[index] = segments[index] * 10 + digit;
+        }
 
         _major = segments[0];
         _minor = segments[1];
